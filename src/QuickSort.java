4d7bb3f054076e7b30573ce3d783b9c3308336d6
JDiegoS <46642689+JDiegoS@users.noreply.github.com>
2019-02-08 18:55:42 -0600

    Update QuickSort.java

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,6 +1,53 @@
 import java.util.Random;
 
 public class QuickSort {
+	
+	private int[] nums;
+	
+	public QuickSort (int[] nums) {
+		this.nums = nums;
+	}
+	
+	
+	public void showResult() {
+		for(int i=0; i<nums.length; ++i) {
+			System.out.println(nums[i]);
+		}
+	}
+	
+	public void quicksort(int low, int high) {
+		if(low >= high) return;
+		
+		int pivot = partition(low, high);
+		quicksort(low, pivot-1);
+		quicksort(pivot+1, high);
+	}
+	public void swap(int i, int j) {
+		int tempo = nums[i];
+		nums[i] = nums[j];
+		nums[j] = tempo;
+	}
+	
+	public void sort() {
+		quicksort(0, nums.length-1);
+	}
+	
+	public int partition(int low, int high) {
+		int pivotIndex = (low+high)/2;
+		swap(pivotIndex,high);
+		
+		int i = low;
+		
+		for(int j=low;j<high;j++) {
+			if(nums[j]<= nums[high]) {
+				swap(i,j);
+				i++;
+				
+			}
+		}
+		swap(i,high);
+		return i;
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -11,7 +58,7 @@ public class QuickSort {
 		for(int j = 0; j<nums.length; j++) {
 			nums[j] = random.nextInt(10000) - 500;
 		}
-		Sort quicksort = new Sort(nums);
+		QuickSort quicksort = new QuickSort(nums);
 		quicksort.sort();
 		quicksort.showResult();
 	}
